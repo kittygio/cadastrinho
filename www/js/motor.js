f1000139c67de7edb4e7f18f28edde4751fae2cd
kittygio <kenmac4t@gmail.com>
2023-05-26 21:26:58 +0900

    26.05.2023

--- www/js/motor.js
+++ www/js/motor.js
@@ -1,2 +1,26 @@
-alert("funcionou !")
+var nome
+var selecao 
+var selecaodisciplina
+var selecaomencao
 
+function cadastrar() {
+    nome = document.getElementById('inputnome').value 
+    document.getElementById('nomealuno').textContent = nome
+
+    selecao = document.getElementById('disciplina')
+    selecaodisciplina = selecao.options[selecao.selectedIndex].value
+
+    selecao = document.getElementById('mencao')
+    selecaomencao = selecao.options[selecao.selectedIndex].value
+
+    localStorage.setItem(selecaodisciplina selecaomencao)
+    )
+
+function listar() {
+    selecao = document.getElementById('disciplina')
+    selecaodisciplina = selecao.options[selecao.selectedIndex].value
+
+    localStorage.getItem(selecaodisciplina)
+
+    document.getElementById('exibirdisciplina').textContent = selecaodisciplina
+}
